4af57cd63bd7c68e9bce7e58085df723b31af85b
J Christopher Phelan <eris@alum.mit.edu>
2016-02-17 19:29:51 -0700

    Mostly working

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -2,102 +2,80 @@
 
 var sumItCtlr = angular.module( 'SumItCtlr', ['Engine'] );
 
-sumItCtlr.controller( "MainMenuCtlr", function( $scope, $location ) {
+sumItCtlr.controller( "MainMenuCtlr", [ "$scope", "$location", "CurrentGame",
+    function( $scope, $location, game ) {
     
     $scope.newGame = function() {
+        game.newGame();
         $location.path( "/equation/" );
     };
    
-});
+}]);
 
-sumItCtlr.controller( "EquationScreenCtlr", function( $scope, $location ) {
-    var i, aDigit;
-    var correctDigits = 3;
+sumItCtlr.controller( "EquationScreenCtlr", [ "$scope", "$location", "CurrentGame", 
+    function( $scope, $location, game ) {
+        
+    var i, aDigit, currentEquation;
     
-    $scope.operand1 = [ '1', '0', '7' ];
-    $scope.operand2 = [ '4', '3', '2' ];
-    
-    $scope.answerDigits = new Array();
-    for( i = 0; i < correctDigits; i++ ) {
-        aDigit = new Object();
-        aDigit.val = "X" + i;
-        aDigit.onDropSuccess = function( data, event ) {
-            this.val = data;
+    var loadEquation = function() {
+        currentEquation = game.game.currentEq();
+        $scope.operand1 = currentEquation.firstOperand;
+        $scope.operand2 = currentEquation.secondOperand;
+        $scope.answerDigits = new Array();
+        for( i = 0; i < currentEquation.corAnsLength(); i++ ) {
+            aDigit = new Object();
+            aDigit.val = null;
+            aDigit.onDropSuccess = function( data, event ) {
+                this.val = data;
+                if( $scope.isAnsComplete() ) {
+                    currentEquation.playerAnswer = parsePlayerAnswer();
+                    nextEquation();
+                }
+            };
+            aDigit.display = function() {
+                return this.val ? this.val : "?";
+            };
+            $scope.answerDigits.push( aDigit );
+        }
+
+        $scope.isAnsComplete = function() {
+            var isCompl = true;
+            for( i = 0; i < $scope.answerDigits.length; i++ ) {
+                isCompl = isCompl && $scope.answerDigits[ i ].val;
+            }
+            return isCompl;
         };
-        $scope.answerDigits.push( aDigit );
-    }
+    };
+    
+    var nextEquation = function() {
+        
+        game.game.next();
+        
+        if( game.game.complete() ) {
+            $location.path("/end/");
+        }
+        
+        loadEquation();
+        
+    };
+    
+    var parsePlayerAnswer = function() {
+        var i, ans = new Array();
+        for( var i = 0; i < $scope.answerDigits.length; i++ ) {
+            ans.push( $scope.answerDigits[ i ].val );
+        }
+        return Number.parseInt( ans.join("") );
+    };
+    
+    loadEquation();
+    
     
     $scope.keypadDigits = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
     
-    $scope.onDropSuccess = function( data, event ) {
-        $scope.userAnswer[ 0 ] = data;
-    };
+}]);
 
-});
+sumItCtlr.controller( "EndOfGameCtlr", ["$scope", "$location", "CurrentGame", 
+    function( $scope, $location, game ) {
+        $scope.equations = game.game;
+}]);
 
-sumItCtlr.controller( "EndOfGameCtlr", function( $scope, $location ) {
-});
-
-//canDoCtlr.controller( "ActListCtlr", [ "$scope", "ActList", 
-//    function( $scope, ActList ) {
-//    /* implement Activity list controller */
-//    $scope.activities = ActList();
-//    
-//    $scope.doneButton = function( act ) {
-//        act.complete();
-//    };
-//    
-//    $scope.deleteButton = function( act ) {
-//        act.cancel();
-//    };
-//    
-//    $scope.postponeButton = function( act ) {
-//        act.postpone();
-//    };
-//
-//    $scope.upButton = function( act ) {
-//        act.raisePrio();
-//    };
-//    
-//    $scope.downButton = function( act ) {
-//        act.lowerPrio();
-//    };
-//    
-//}]);
-//
-//canDoCtlr.controller( "NewActFormCtlr", [ "$scope", "ActList", "Activity",
-//    function( $scope, ActList, Activity ) {
-//
-//    var addActivity;
-//    
-//    $scope.newActData = {};
-//            
-//    addActivity = function() {
-//        var act = new Activity( $scope.newActData );
-//        ActList().push( act );
-//        return act;
-//    };
-//    
-//    $scope.actNameEnter = function( keyCode ) {
-//        var act;
-//        if ( keyCode === 13 ) {
-//            act = addActivity().queue();
-//            if ( $scope.viewMode === "backlog" ) {
-//                act.postpone();
-//            }
-//        }
-//    };
-//    
-//    $scope.queueButton = function() {
-//        addActivity().queue();
-//    };
-//    
-//    $scope.workButton = function() {
-//        addActivity().queue().doWork();
-//    };
-//    
-//    $scope.backlogButton = function() {
-//        addActivity().queue().postpone();
-//    };
-//    
-//}]);
--- public_html/js/engine.js
+++ public_html/js/engine.js
@@ -4,65 +4,98 @@ var engine = angular.module( 'Engine', [] );
 
 engine.constant( 'GameConstants', 
     {
-        'ProblemCount': 5,
-        'OperandMinSize': 2,
-        'OperandMaxSize': 5
+        'ProblemCount': 2,
+        'OperandMin': 10,
+        'OperandMax': 100 // 100000
     }
 );
 
-engine.service( 'GameFunctions', [ 'GameConstants', function( gameConstants ) {
-        var gameFunctions = new Object();
-        gameFunctions.makeOperand = function() {
-            return 277;
+engine.service( 'Utilities', [ 'GameConstants', function( gameConstants ) {
+        var utilities = new Object();
+        utilities.operandFactory = function() {
+            var operand = new Array();
+            operand.value = gameConstants.OperandMin + Math.floor( Math.random() *
+                    ( gameConstants.OperandMax - gameConstants.OperandMin ) );
+            operand.text = operand.value.toString();
+            for( var i = 0; i < operand.text.length; i++ ) {
+                var dig = new Object();
+                dig.val = operand.text.charAt( i );
+                operand.push( dig );
+            }
+            return operand;
         };
-        gameFunctions.drillFunction = function( a, b ) {
+        utilities.drillFunction = function( a, b ) {
             return a + b;
         };
-        return gameFunctions;
+        return utilities;
 }]);
 
-engine.service( 'Utilities', function() {
-    var utilities = new Object();
-    utilities.digits = function( number ) {
-        return [ '3', '9', '4', '2' ];
-    };
-    utilities.number = function( digits ) {
-        return 7904;
-    };
-    return utilities;
-});
-
-engine.factory( 'Equation', [ 'GameFunctions', function( gameFunctions ) {
-        var equation = new Object();
-        equation.firstOperand = gameFunctions.makeOperand();
-        equation.secondOperand = gameFunctions.makeOperand();
-        equation.actualResult = gameFunctions.drillFunction( equation.firstOperand, 
-            equation.secondOperand );
-        equation.correctAnswer = function() {
-            return ( equation.playerAnswer === equation.actualResult );
+engine.service( 'CurrentGame', [ 'Game', function( Game ) {
+        var currentGame = new Object();
+        currentGame.newGame = function() {
+            currentGame.game = new Game();
         };
-        return equation;
+        return currentGame;
+}]);
+
+
+engine.factory( 'Equation', [ 'Utilities', function( utilities ) {
+        return function() {
+            var equation = new Object();
+            equation.firstOperand = utilities.operandFactory();
+            equation.secondOperand = utilities.operandFactory();
+            equation.actualAnswer = utilities.drillFunction( 
+                    equation.firstOperand.value,
+                    equation.secondOperand.value );
+            equation.displayAnswer = new Array();
+            equation.displayAnswer.text = equation.actualAnswer.toString();
+            for( var i = 0; i < equation.displayAnswer.text.length; i++ ) {
+                var dig = new Object();
+                dig.val = equation.displayAnswer.text.charAt( i );
+                equation.displayAnswer.push( dig );
+            }
+            equation.playerAnswerDisplay= function() {
+                var pAD = new Array();
+                var pADText = equation.playerAnswer.toString();
+                for( var i = 0; i < pADText.length; i++ ) {
+                    var dig = new Object();
+                    dig.val = pADText.charAt( i );
+                    pAD.push( dig );
+                }
+                return pAD;
+            };
+            equation.isAnswerCorrect = function() {
+                return ( this.playerAnswer === this.actualAnswer );
+            };
+            equation.corAnsLength = function() {
+                return this.actualAnswer.toString().length;
+            };
+            return equation;
+        };
 }]);
 
 engine.factory( 'Game', [ 'Equation', 'GameConstants', 
-    function( equation, gameConstants ) {
-        var i;
-        var c = false;
-        var game = new Array();
-        for( i = 0; i < gameConstants.ProblemCount; i++ ) {
-            game.push( new equation() );
-        }
-        game.index = 0;
-        game.next = function() {
-            game.index++;
-            if( game.index >= game.length ) {
-                c = true;
+    function( Equation, gameConstants ) {
+        return function() {
+            var i;
+            var c = false;
+            var game = new Array();
+            for( i = 0; i < gameConstants.ProblemCount; i++ ) {
+                game.push( new Equation() );
             }
-        };
-        game.current = function() {
-            return game[ game.index ];
-        };
-        game.complete = function() {
-            return c;
+            game.index = 0;
+            game.next = function() {
+                this.index++;
+                if( this.index >= this.length ) {
+                    c = true;
+                }
+            };
+            game.currentEq = function() {
+                return this[ this.index ];
+            };
+            game.complete = function() {
+                return c;
+            };
+            return game;
         };
 }]);
